3b14465d80a06040847a501f9a78d266b6c9af86
FrauBienenstich <susanne.dewein@gmail.com>
2016-01-17 17:07:14 +0100

    start implementing intervals for connected/disconnected WIP

--- tests/unit/start-checking.js
+++ tests/unit/start-checking.js
@@ -61,3 +61,63 @@ test('startChecking() with interval & 500 error response', function (t) {
   simple.restore()
   clock.uninstall()
 })
+
+
+test('options.interval.connected & options.interval.disconnected', function (t) {
+  t.plan(2)
+
+  var clock = lolex.install(0)
+  var emitter = {emit: function () {}}
+
+  var state = {
+   method: 'HEAD',
+   url: 'https://example.com/ping'
+  }
+
+  simple.mock(startChecking.internals, 'check').callFn(function () {
+   return { then: function (success) { success() } }
+  })
+
+  startChecking(state, {
+   interval: { connected: 30000, disconnected: 3000 }
+  })
+
+  t.ok(state.interval, 'state.interval is set')
+  clock.tick(70000)
+  t.is(startChecking.internals.check.callCount, 2, '2 requests sent')
+
+  clearTimeout(state.interval)
+  simple.restore()
+  clock.uninstall()
+})
+
+test.only('options.interval.connected & options.interval.disconnected', function (t) {
+  t.plan(2)
+
+  var clock = lolex.install(0)
+  var emitter = {emit: function () {}}
+
+  var state = {
+   method: 'HEAD',
+   url: 'https://example.com/ping'
+  }
+
+  simple.mock(startChecking.internals, 'check').callFn(function () {
+   return { then: function (success) { success() } }
+  })
+
+  simple.mock(startChecking.internals, 'ok', false);
+
+  startChecking(state, {
+   interval: { connected: 30000, disconnected: 3000 }
+  })
+
+  t.ok(state.interval, 'state.interval is set')
+  clock.tick(7000)
+  t.is(startChecking.internals.check.callCount, 2, '2 requests sent')
+
+  clearTimeout(state.interval)
+  simple.restore()
+  clock.uninstall()
+})
+
